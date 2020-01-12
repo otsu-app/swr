adbd3d86e4d4a959c67fbb120f5ed926a6ba5fcd
justsee <justin@digitalally.com.au>
2020-01-12 15:02:13 +0800

    Handle undefined to return correct state for isReachingEnd.

--- src/use-swr-pages.tsx
+++ src/use-swr-pages.tsx
@@ -146,7 +146,7 @@ export function useSWRPages<OffsetType = any, Data = any, Error = any>(
   }, deps)
 
   // Doesn't have a next page
-  const isReachingEnd = pageOffsets[pageCount] === null
+  const isReachingEnd = pageOffsets[pageCount] == null
   const isLoadingMore = pageCount === pageOffsets.length
   const isEmpty = isReachingEnd && pageCount === 1 && emptyPageRef.current
   const loadMore = useCallback(() => {
